c00f30273fe923a629f01391f59042eb897eabe4
bigdata <logFM3798@gmail.com>
2016-07-11 22:03:21 +0800

    Update eye_social_server.bash

--- eye_social_server.bash
+++ eye_social_server.bash
@@ -4,13 +4,15 @@ mkdir r_data
 
 # ES
 docker run -d --name es_QQ_master -p 9200:9200 -p 9300:9300 jeffsheng/es_labber -Des.node.name="QQ_master"
-
+docker run -d --name es_QQ_nodeI  --link es_QQ_master:Elas_Mas -p 9201:9200 -p 9301:9300 jeffsheng/es_labber 
+  -Des.node.name="QQ_node-I" -Des.discovery.zen.ping.unicast.hosts="Elas_Mas"
 echo "ES container done."
 sleep 2
 
 # Redis
 docker run -d --name redis_server -v $PWD/r_data:/data redis redis-server --appendonly yes
 
+ 
 echo "Redis container done."
 sleep 2
 # Web-Server
