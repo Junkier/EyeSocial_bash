f51cdcb09a67259bd8d216da6ffa6025914ed710
Junkier <logFM3798@gmail.com>
2016-07-21 12:59:47 +0800

    Es_cluster build

--- eye_social_server.bash
+++ eye_social_server.bash
@@ -3,20 +3,20 @@
 mkdir r_data
 
 # ES
-docker run -d --name es_QQ_master -p 9200:9200 -p 9300:9300 jeffsheng/es_labber -Des.node.name="QQ_master"
+docker run -d --name es_QQ_master -p 9200:9200 -p 9300:9300 jeffsheng/es_labber -Des.node.name="QQ_master"\
   -Des.cluster.name="EyeSocial_cluster for PhoneChiu"
-docker run -d --name es_QQ_nodeI  --link es_QQ_master:Elas_Mas -p 9201:9200 -p 9301:9300 jeffsheng/es_labber 
+docker run -d --name es_QQ_nodeI  --link es_QQ_master:Elas_Mas -p 9201:9200 -p 9301:9300 jeffsheng/es_labber\
   -Des.node.name="QQ_node-I" -Des.discovery.zen.ping.unicast.hosts="Elas_Mas"
-docker run -d --name es_QQ_nodeII --link es_QQ_master:Elas_Mas -p 9202:9200 -p 9302:9300 jeffsheng/es_labber
-  -Des.node.name="QQ_node-II" -Des.discovery.zen.ping.unicast.hosts="Elas_Mas"  
-  
+docker run -d --name es_QQ_nodeII --link es_QQ_master:Elas_Mas -p 9202:9200 -p 9302:9300 jeffsheng/es_labber\
+  -Des.node.name="QQ_node-II" -Des.discovery.zen.ping.unicast.hosts="Elas_Mas"
+
 echo "ES container done."
 sleep 2
 
 # Redis
 docker run -d --name redis_server -v $PWD/r_data:/data redis redis-server --appendonly yes
 
- 
+
 echo "Redis container done."
 sleep 2
 # Web-Server
