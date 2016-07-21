f08a7068e24973f0231a8f0177f604eb1028581c
Junkier <logFM3798@gmail.com>
2016-07-21 20:04:09 +0800

    2016-07-21 webserver refresh

--- eye_social_server.bash
+++ eye_social_server.bash
@@ -20,7 +20,7 @@ docker run -d --name redis_server -v $PWD/r_data:/data redis redis-server --appe
 echo "Redis container done."
 sleep 2
 # Web-Server
-docker run -d --name webserver --link es_QQ_master:Elas_Mas --link redis_server:R_Server -p 32767:32767 jeffsheng/node_labber
+docker run -d --name webserver --link es_QQ_master:Elas_Mas --link redis_server:R_Server -p 32767:32767 jeffsheng/node_centos_labber
 
 echo "Node.js Webserver done."
 echo "Done!!"
