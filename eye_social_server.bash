8f85f3747212ad64e6cd26c3c8f4b239a440f9dc
Junkier <logFM3798@gmail.com>
2016-08-31 15:45:38 +0800

    fix file.

--- eye_social_server.bash
+++ eye_social_server.bash
@@ -3,6 +3,7 @@
 mkdir r_data
 
 # ES - Heap_size:2g
+# 跨主機需用 --net=host 放在同一個網段
 docker run -d --name es_QQ_master -e ES_HEAP_SIZE=2g -p 9200:9200 -p 9300:9300 jeffsheng/es_labber -Des.node.name="QQ_master" \
   -Des.cluster.name="EyeSocial_cluster for PhoneChiu"
 docker run -d --name es_QQ_nodeI  -e ES_HEAP_SIZE=2g --link es_QQ_master:Elas_Mas -p 9201:9200 -p 9301:9300 jeffsheng/es_labber \
@@ -26,4 +27,12 @@ echo "Node.js Webserver done."
 echo "Done!!"
 
 
+
+# ES 跨主機
+docker run -d --name es_QQ_master -e ES_HEAP_SIZE=2g --net=host -p 9200:9200 -p 9300:9300 jeffsheng/es_labber -Des.node.name="QQ_master" \
+  -Des.cluster.name="EyeSocial_cluster for AirforceI"
+docker run -d --name es_QQ_nodeI  -e ES_HEAP_SIZE=2g --net=host -p 9200:9200 -p 9300:9300 jeffsheng/es_labber \
+  -Des.cluster.name="EyeSocial_cluster for AirforceI" -Des.node.name="QQ_node-I" -Des.discovery.zen.ping.unicast.hosts="master_ip"
+docker run -d --name es_QQ_nodeII -e ES_HEAP_SIZE=2g  -p 9200:9200 -p 9300:9300 jeffsheng/es_labber \
+  -Des.cluster.name="EyeSocial_cluster for AirforceI" -Des.node.name="QQ_node-II" -Des.discovery.zen.ping.unicast.hosts="master_ip"
 
